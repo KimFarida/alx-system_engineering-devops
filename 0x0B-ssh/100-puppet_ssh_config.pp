13eb5c9c2b40361dde6d3aa2c8ce61d8660e18a4
KimFarida <farimomoh@gmail.com>
2022-10-17 11:09:10 -0700

    Task 100 Adv

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,12 @@
- off password authentication and uses holberton key
- file_line { 'Turn off passwd auth':
-	   ensure => 'present',
-		    path   => '/etc/ssh/ssh_config',
-		      line   => '    PasswordAuthentication no',
- }
+# Turns off password authentication and uses school key
+file_line { 'Turn off passwd auth':
+	  ensure => 'present',
+		   path   => '/etc/ssh/ssh_config',
+		     line   => '    PasswordAuthentication no',
+}
 
 file_line { 'Declare identity file':
 	  ensure => 'present',
 		   path   => '/etc/ssh/ssh_config',
 		     line   => '    IdentityFile ~/.ssh/school',
 }
-
